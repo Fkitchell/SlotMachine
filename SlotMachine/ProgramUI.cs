18260b3d03b82822184a6b87b99dafcec828961b
Fletcher Kitchell <fdkitchell@gmail.com>
2020-05-14 22:02:42 -0400

    edited cw for telling user how to quit game.

--- SlotMachine/ProgramUI.cs
+++ SlotMachine/ProgramUI.cs
@@ -113,7 +113,7 @@ namespace SlotMachine
 
 
                 Console.WriteLine($"Your new total is ${moneyWallet}.\n" +
-                    $"Press Q to end the game, or any other key to continue.");
+                    $"Type Q then enter to end the game, or any simply press enter to continue.");
                 string continueInput = Console.ReadLine();
                 if (continueInput.ToLower() == "q")
                 {
